f7fa9586a2399fbfc4f8101b6cddff88eaf051cd
Anmfishe <anmfishe@ucsc.edu>
2014-11-22 14:18:01 -0800

    mainjs now ignores context menu, listens to right mouse up and down, and will start zooming out (placeholder functionality)

--- src/main.js
+++ src/main.js
@@ -8,6 +8,7 @@ var Main = function(w, h){
 	this.scene = new THREE.Scene();
 
 	this.selected; //selected game element
+	this.rightMouseDown;
 
 }
 
@@ -16,6 +17,7 @@ Main.prototype.init = function(){
 	document.body.appendChild(this.renderer.domElement);
 	this.renderer.setClearColor(0x222222, 1.0);
 	this.renderer.clear();
+	this.rightMouseDown = false;
 
 	this.camera.position.x = 0;
 	this.camera.position.y = 0;
@@ -24,15 +26,37 @@ Main.prototype.init = function(){
 	this.scene.add(new THREE.AmbientLight(0xeeeeee));
 
 	var g = new GameObject(500, 0, 0, "http://upload.wikimedia.org/wikipedia/en/thumb/4/42/Supersmashbox.jpg/250px-Supersmashbox.jpg", this.scene);
+	var that = this;
 
 	this.selected = g;
 
 	this.renderer.render(this.scene, this.camera);
+	document.addEventListener("contextmenu", function(e){
+		e.preventDefault();
+	});
+
+
+	document.addEventListener("mousedown", function(e){
+		if(e.which == "3"){
+		that.rightMouseDown = true;
+		}
+	});
+
+	document.addEventListener("mouseup", function(e){
+		if(e.which == "3"){
+		that.rightMouseDown = false;
+		}
+	});
+
 }
 
 Main.prototype.update = function(){
 	this.camera.lookAt(new THREE.Vector3(500, 0, 0));
 	this.selected.rotate();
 	this.renderer.render(this.scene, this.camera);
+	if(this.rightMouseDown){
+		this.camera.position.z += 1;
+		console.log("Finally");
+	}
 
 }
