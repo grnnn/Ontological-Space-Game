10050a6663eaffa146abd6dce5893230b7e0d49e
Anmfishe <anmfishe@ucsc.edu>
2014-11-23 14:41:23 -0800

    Commented a bunch of code which we should start doing to see what functionality something we changed has. Getting ready to rotate that camera with MATH

--- src/main.js
+++ src/main.js
@@ -8,7 +8,7 @@ var Main = function(w, h){
 	this.scene = new THREE.Scene();
 
 	this.selected; //selected game element
-	this.rightMouseDown;
+	this.rightMouseDown; //is right mouse down
 
 }
 
@@ -17,46 +17,58 @@ Main.prototype.init = function(){
 	document.body.appendChild(this.renderer.domElement);
 	this.renderer.setClearColor(0x222222, 1.0);
 	this.renderer.clear();
+
+	//mouse button isn't down
 	this.rightMouseDown = false;
 
+	//set camera position
 	this.camera.position.x = 0;
 	this.camera.position.y = 0;
 	this.camera.position.z = 0;
 
 	this.scene.add(new THREE.AmbientLight(0xeeeeee));
 
+	//create the game object
 	var g = new GameObject(500, 0, 0, "http://upload.wikimedia.org/wikipedia/en/thumb/4/42/Supersmashbox.jpg/250px-Supersmashbox.jpg", this.scene);
+
+	//get a reference to this
 	var that = this;
 
+	//set selected object, only like this for testing
 	this.selected = g;
 
+	//set what the camera is looking at
+	//we would change this when we are "selected" or not
+	this.camera.lookAt(new THREE.Vector3(this.selected.x, this.selected.y, this.selected.z));
 	this.renderer.render(this.scene, this.camera);
+
+	//context menu and mouse event listenders
 	document.addEventListener("contextmenu", function(e){
 		e.preventDefault();
 	});
-
-
 	document.addEventListener("mousedown", function(e){
 		if(e.which == "3"){
 		that.rightMouseDown = true;
 		}
 	});
-
 	document.addEventListener("mouseup", function(e){
 		if(e.which == "3"){
 		that.rightMouseDown = false;
 		}
 	});
-
 }
 
 Main.prototype.update = function(){
-	this.camera.lookAt(new THREE.Vector3(500, 0, 0));
+	//rotate the selected object on update
 	this.selected.rotate();
+
+	//render on update
 	this.renderer.render(this.scene, this.camera);
+
+	//what to do when mouse right is held down
 	if(this.rightMouseDown){
-		this.camera.position.z += 1;
-		console.log("Finally");
+		//placeholder functionality, needs to rotate around object based on mouse movements
+		this.camera.position.x -= 5;
 	}
 
 }
