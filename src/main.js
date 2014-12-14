3caf020cb16e77d0ff98d36ba8783358b207d5c8
unknown <towenmilner@gmail.com>
2014-12-13 18:57:14 -0800

    selection of different games finally works.

--- src/main.js
+++ src/main.js
@@ -25,7 +25,6 @@ var Main = function(w, h, gameFile){
 	this.xAngle = 0; //track rotation of camera x
 	this.yAngle = -Math.PI/2; //track rotation of camera y
 
-	this.raycaster = new THREE.Raycaster(); //raycaster for selecting objects
 	this.rayVector = new THREE.Vector3(); //utility vector for raycaster
 
 	this.startVector = new THREE.Vector3(1000, 0, 0);
@@ -75,16 +74,30 @@ Main.prototype.init = function(){
 	});
 	document.addEventListener("mousedown", function(e){
 		if(e.which == "1"){
-			that.rayVector.set(that.mousePos.x, that.mousePos.y, 0.1).unproject(that.camera);
-			that.raycaster.ray.set(that.camera.position, that.rayVector.sub(that.camera.position).normalize() );
+			that.rayVector.set((that.mousePos.x/window.innerWidth) * 2 - 1, -(that.mousePos.y/window.innerHeight) * 2 + 1, 0.5).unproject(that.camera);
+			that.rayVector.sub(that.camera.position).normalize();
 
-			var intersection = that.raycaster.intersectObjects( that.gameSquares )[0];
+			var raycaster = new THREE.Raycaster();
+			raycaster.ray.set(that.camera.position, that.rayVector);
 
-			if(intersection !== null){
-				var gameObj = that.squareHash[JSON.stringify(intersection)];
-				that.selected = gameObj;
-				$("#myimage").attr("style", "height:200px;width:200px;border:0 none;background-color:transparent;position:absolute");
+			var intersections = raycaster.intersectObjects(that.gameSquares);
+
+			if(intersections[0] !== undefined){
+				that.selected = that.squareHash[intersections[0].object.wiki];
+				that.hasRightPressed = false;
+				that.startVector = new THREE.Vector3(that.selected.x + 1000, that.selected.y, that.selected.z);
 			}
+
+			/*for(var i = 0; i < that.gameSquares.length; i++){
+				console.log(raycaster.intersectObject(that.gameSquares[i]).length);
+				if(raycaster.intersectObject(that.gameSquares[i]).length > 0){
+					var gameObj = that.squareHash[JSON.stringify(intersection)];
+
+					that.selected = gameObj;
+					$("#myimage").attr("style", "height:200px;width:200px;border:0 none;background-color:transparent;position:absolute");
+					console.log(that.selected);
+				}
+			}*/
 		}
 
 
@@ -105,9 +118,9 @@ Main.prototype.init = function(){
 		that.rightMouseDown = false;
 		}
 	});
-	document.onscroll = function(){
+	/*document.onscroll = function(){
 		console.log("blah");
-	};
+	};*/
 	document.addEventListener("keypress", function(e){
 		if(e.which == "119"){
 			that.pushZoom(-1);
@@ -122,20 +135,20 @@ Main.prototype.init = function(){
 Main.prototype.update = function(){
 	if(this.targetGames === this.gamesLoaded){
 		//rotate around the selected object on update, only if the right mouse button hasn't been clicked for that object
-		if(!this.hasRightPressed){
+		if(!this.hasRightPressed && this.selected !== null){
 			this.pushRotateCamera(0.001, 0, this.selected);
 		}
 
 		//what to do when mouse right is held down:
 		//Get force of angle "push" from difference between current mouse pos and starting mouse pos
 		//We cap the movement of it so that, when distance is increased, the rotation doesn't increase dramatically
-		if(this.rightMouseDown){
+		if(this.rightMouseDown && this.selected !== null){
 			var xMovement = (this.rightLocation.x - this.mousePos.x)/10000;
-			var yMovement = -(this.rightLocation.y - this.mousePos.y)/10000;
-			if(xMovement > 0.02) xMovement = 0.02;
-			if(xMovement < -0.02) xMovement = -0.02;
-			if(yMovement > 0.02) yMovement = 0.02;
-			if(yMovement < -0.02) yMovement = -0.02;
+			var yMovement = (this.rightLocation.y - this.mousePos.y)/10000;
+			if(xMovement > 0.02) xMovement = 0.03;
+			if(xMovement < -0.02) xMovement = -0.03;
+			if(yMovement > 0.02) yMovement = 0.01;
+			if(yMovement < -0.02) yMovement = -0.01;
 			this.pushRotateCamera(xMovement, yMovement, this.selected);
 			
 		}
@@ -162,9 +175,9 @@ Main.prototype.pushRotateCamera = function(pushX, pushY, position){
 
 	//This algorithm was taken from the "OrbitControls.js" package that is included with three.js.
 	//Given the new angles of rotation, this is how we calculate the offset coordinates of the camera
-	var offSetX = 500*Math.sin(this.xAngle)*Math.sin(this.yAngle);
-	var offSetY = 500*Math.cos(this.yAngle);
-	var offSetZ = 500*Math.sin(this.yAngle)*Math.cos(this.xAngle);
+	var offSetX = 1000*Math.sin(this.xAngle)*Math.sin(this.yAngle);
+	var offSetY = 1000*Math.cos(this.yAngle);
+	var offSetZ = 1000*Math.sin(this.yAngle)*Math.cos(this.xAngle);
 
 	//Offset coordinates are simply added to position to get camera coordinates
 	this.startVector.x = position.x + offSetX;
@@ -189,15 +202,15 @@ Main.prototype.pushZoom = function(push){
 	if(this.zoom < 0.1) this.zoom = 0.1;
 
 	if(this.selected !== null){
-		this.camera.position.x = this.zoom * (this.selected.x - this.startVector.x);
-		this.camera.position.y = this.zoom * (this.selected.y - this.startVector.y);
-		this.camera.position.z = this.zoom * (this.selected.z - this.startVector.z);
+		this.camera.position.x = this.startVector.x + this.zoom * (this.selected.x - this.startVector.x);
+		this.camera.position.y = this.startVector.y + this.zoom * (this.selected.y - this.startVector.y);
+		this.camera.position.z = this.startVector.z + this.zoom * (this.selected.z - this.startVector.z);
 		//Make sure the camera is looking at the position
     	this.camera.lookAt(new THREE.Vector3(this.selected.x, this.selected.y, this.selected.z));
 	} else {
-		this.camera.position.x = this.zoom * (this.endVector.x - this.startVector.x);
-		this.camera.position.y = this.zoom * (this.endVector.y - this.startVector.y);
-		this.camera.position.z = this.zoom * (this.endVector.z - this.startVector.z);
+		this.camera.position.x = this.startVector.x + this.zoom * (this.endVector.x - this.startVector.x);
+		this.camera.position.y = this.startVector.y + this.zoom * (this.endVector.y - this.startVector.y);
+		this.camera.position.z = this.startVector.z + this.zoom * (this.endVector.z - this.startVector.z);
 		//Make sure the camera is looking at the position
     	this.camera.lookAt(new THREE.Vector3(this.endVector.x, this.endVector.y, this.endVector.z));
 	}
@@ -215,9 +228,12 @@ Main.prototype.readGames = function(gameFile){
 			var myGame = data[i];
 			var obj = new GameObject(myGame.x, myGame.y, myGame.z, myGame.gameTitle, myGame.wikipedia, myGame.boxArt, that.scene);
 
+			//set an arbitrary identifier for hashing, make it the wiki link, since that's already unique and garaunteed
+			obj.cube.wiki = obj.wiki;
+
 			//add mesh to gameSquares and add mesh to hash
 			that.gameSquares.push(obj.cube);
-			that.squareHash[JSON.stringify(obj.cube)] = obj;
+			that.squareHash[obj.cube.wiki] = obj;
 
 
 			//set first obj to selected, temporary
